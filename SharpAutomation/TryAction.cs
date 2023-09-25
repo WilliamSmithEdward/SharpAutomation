2ff0ebb52b02781c596ad6800ae24cb4d5d2148a
William Smith <williamsmithe@icloud.com>
2023-09-25 16:31:41 -0700

    Add project files.

---
+++ SharpAutomation/TryAction.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpAutomation
+{
+    public static class TryAction
+    {
+        public static async Task<Exception?> RunAsync(Action action)
+        {
+            try
+            {
+                await Task.Run(action);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+        }
+    }
+}
