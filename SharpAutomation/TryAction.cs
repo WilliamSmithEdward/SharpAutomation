77f7811c0ad645c4e8c1a752e5a0008b43f245e3
William Smith <williamsmithe@icloud.com>
2023-09-26 15:34:37 -0700

    modify parameter ordering for the TryAction.RunAsync method

--- SharpAutomation/TryAction.cs
+++ SharpAutomation/TryAction.cs
@@ -9,13 +9,13 @@
         /// Executes an action asynchronously and captures any exceptions that may occur.
         /// </summary>
         /// <param name="action">The action to be executed.</param>
-        /// <param name="_exceptionList">An optional list to which exceptions will be added in case of errors.</param>
         /// <param name="retries">The number of times to retry the action in case of exceptions (default is 0).</param>
         /// <param name="waitBetweenTriesSeconds">The wait time in seconds between retries (default is 0).</param>
+        /// <param name="_exceptionList">An optional list to which exceptions will be added in case of errors.</param>
         /// <returns>
         /// A boolean value that represents whether the action was ultimately executed successfully.
         /// </returns>
-        public static async Task<bool> RunAsync(Action action, List<Exception>? _exceptionList = null, int retries = 0, int waitBetweenTriesSeconds = 0)
+        public static async Task<bool> RunAsync(Action action, int retries = 0, int waitBetweenTriesSeconds = 0, List<Exception>? _exceptionList = null)
         {
             int retryCount = 0;
 
